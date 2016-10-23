27b161593149bf631a63e3efd5c2775752afb026
Astos <hdpenney@gmail.com>
2016-10-23 14:12:15 -0500

    removed extraneous markers for polling

--- js/markers.js
+++ js/markers.js
@@ -9,20 +9,14 @@ var Natives = L.icon({iconUrl: 'img/marker/Natives.png',iconSize: [34, 53],iconA
 var Epitath = L.icon({iconUrl: 'img/marker/Epitath.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 var Epitath1 = L.icon({iconUrl: 'img/marker/Epitath1.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 var Epitath2 = L.icon({iconUrl: 'img/marker/Epitath2.png',iconSize: [34, 53],iconAnchor: [17, 53]})
-var Epitath3 = L.icon({iconUrl: 'img/marker/Epitath.png',iconSize: [43, 66],iconAnchor: [22, 66]})
-var Epitath4 = L.icon({iconUrl: 'img/marker/Epitath1.png',iconSize: [43, 66],iconAnchor: [22, 66]})
-var Epitath5 = L.icon({iconUrl: 'img/marker/Epitath2.png',iconSize: [43, 66],iconAnchor: [22, 66]})
 
 // Define the markers
 var markers_Array = [
 
-	[59.44887, -24.32432,Natives,"<b>Test Marker</b>"],
+	/*[59.44887, -24.32432,Natives,"<b>Test Marker</b>"],*/
 	[33.66787, 114.67181,Epitath,"<b>Epitath</b>"],
 	[33.66787, 124.67181,Epitath2,"<b>Epitath</b>"],
-	[33.66787, 134.67181,Epitath1,"<b>Epitath</b>"],
-	[51.61465, 114.67181,Epitath3,"<b>Epitath</b>"],
-	[51.61465, 124.67181,Epitath5,"<b>Epitath</b>"],
-	[51.61465, 134.67181,Epitath4,"<b>Epitath</b>"]];
+	[33.66787, 134.67181,Epitath1,"<b>Epitath</b>"]];
 
 
 
