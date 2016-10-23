69caa72d807c381b6df76d40471747c3d06c1af3
Astos <hdpenney@gmail.com>
2016-10-23 13:36:19 -0500

    size change lineup

--- js/markers.js
+++ js/markers.js
@@ -6,17 +6,17 @@
 // For each region add an image to 'img/marker/' 
 var Natives = L.icon({iconUrl: 'img/marker/Natives.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
-var Epitath1 = L.icon({iconUrl: 'img/marker/Epitath1.png',iconSize: [34, 53],iconAnchor: [17, 53]})
-var Epitath2 = L.icon({iconUrl: 'img/marker/Epitath2.png',iconSize: [34, 53],iconAnchor: [17, 53]})
-var Epitath3 = L.icon({iconUrl: 'img/marker/Epitath3.png',iconSize: [34, 53],iconAnchor: [17, 53]})
+var Epitath1 = L.icon({iconUrl: 'img/marker/Epitath1.png',iconSize: [30, 46],iconAnchor: [15, 46]})
+var Epitath2 = L.icon({iconUrl: 'img/marker/Epitath2.png',iconSize: [26, 40],iconAnchor: [14, 40]})
+var Epitath = L.icon({iconUrl: 'img/marker/Epitath.png',iconSize: [23, 35],iconAnchor: [12, 35]})
 
 // Define the markers
 var markers_Array = [
 
 	[59.44887, -24.32432,Natives,"<b>Test Marker</b>"],
-	[33.66787, 114.67181,Epitath1,"<b>Epitath</b>"],
-	[33.23298, 125.10039,Epitath2,"<b>Epitath</b>"],
-	[34.10058, 136.66216,Epitath3,"<b>Epitath</b>"]];
+	[33.66787, 114.67181,Epitath,"<b>Epitath</b>"],
+	[33.66787, 124.67181,Epitath2,"<b>Epitath</b>"],
+	[33.66787, 134.67181,Epitath1,"<b>Epitath</b>"]];
 
 
 
