4de55e53ae5c00f75ad45cb9977b56fe6d02bf89
Astos <hdpenney@gmail.com>
2016-10-26 13:04:43 -0500

    Finalized Epitath marker, and added a few american fort test markers.

--- js/markers.js
+++ js/markers.js
@@ -7,16 +7,20 @@
 var Natives = L.icon({iconUrl: 'img/marker/Natives.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
 var Epitath = L.icon({iconUrl: 'img/marker/Epitath.png',iconSize: [34, 53],iconAnchor: [17, 53]})
-var Epitath1 = L.icon({iconUrl: 'img/marker/Epitath1.png',iconSize: [34, 53],iconAnchor: [17, 53]})
-var Epitath2 = L.icon({iconUrl: 'img/marker/Epitath2.png',iconSize: [34, 53],iconAnchor: [17, 53]})
+
+var Pistol = L.icon({iconUrl: 'img/marker/Pistol.png',iconSize: [34, 53],iconAnchor: [17, 53]})
+var Rifles = L.icon({iconUrl: 'img/marker/Rifles.png',iconSize: [34, 53],iconAnchor: [17, 53]})
+var Cannon = L.icon({iconUrl: 'img/marker/Cannon.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
 // Define the markers
 var markers_Array = [
 
 	/*[59.44887, -24.32432,Natives,"<b>Test Marker</b>"],*/
 	[33.66787, 114.67181,Epitath,"<b>Epitath</b>"],
-	[33.66787, 124.67181,Epitath2,"<b>Epitath</b>"],
-	[33.66787, 134.67181,Epitath1,"<b>Epitath</b>"]];
+	[40.58522, 58.37838,Pistol,"<b>Fort Major</b>"],
+	[40.58522, 68.37838,Rifles,"<b>Fort Major</b>"],
+	[40.58522, 78.37838,Cannon,"<b>Fort Major</b>"]
+	];
 
 
 
