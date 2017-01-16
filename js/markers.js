4f6e185bbaed822bf70c68251a8f7eb5e3b6cddc
Astos <hdpenney@gmail.com>
2017-01-16 16:28:38 -0600

    second Juaros test

--- js/markers.js
+++ js/markers.js
@@ -13,6 +13,7 @@ var Rifles = L.icon({iconUrl: 'img/marker/Rifles.png',iconSize: [34, 53],iconAnc
 var FortMajor = L.icon({iconUrl: 'img/marker/Cannon.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
 var Juaros = L.icon({iconUrl: 'img/marker/Juaros.png',iconSize: [34,53],iconAnchor: [17, 53]})
+var Juaros2 = L.icon({iconUrl: 'img/marker/Juaros2.png',iconSize: [34,53],iconAnchor: [17, 53]})
 
 // Define the markers
 var markers_Array = [
@@ -20,7 +21,8 @@ var markers_Array = [
 	/*[59.44887, -24.32432,Natives,"<b>Test Marker</b>"],*/
 	[33.66787, 114.67181,Epitaph,"<b>Epitaph</b>"],
 	[40.58522, 58.87838,FortMajor,"<b>Fort Major</b>"],
-	[-66.30466, 81.83398,Juaros,"<b>Juaros</b>"]
+	[-66.30466, 81.83398,Juaros,"<b>Juaros</b>"],
+	[-66.30466, 87.91506,Juaros2,"<b>Juaros</b>"]
 	];
 
 
