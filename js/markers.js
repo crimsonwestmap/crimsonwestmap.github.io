0e5c8315acc6755f2d5abc261461236cadf7ba96
Astos <hdpenney@gmail.com>
2017-01-16 14:22:37 -0600

    Minor update patch

--- js/markers.js
+++ js/markers.js
@@ -6,20 +6,18 @@
 // For each region add an image to 'img/marker/' 
 var Natives = L.icon({iconUrl: 'img/marker/Natives.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
-var Epitath = L.icon({iconUrl: 'img/marker/Epitath.png',iconSize: [34, 53],iconAnchor: [17, 53]})
+var Epitaph = L.icon({iconUrl: 'img/marker/Epitaph.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
 var Pistol = L.icon({iconUrl: 'img/marker/Pistol.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 var Rifles = L.icon({iconUrl: 'img/marker/Rifles.png',iconSize: [34, 53],iconAnchor: [17, 53]})
-var Cannon = L.icon({iconUrl: 'img/marker/Cannon.png',iconSize: [34, 53],iconAnchor: [17, 53]})
+var FortMajor = L.icon({iconUrl: 'img/marker/Cannon.png',iconSize: [34, 53],iconAnchor: [17, 53]})
 
 // Define the markers
 var markers_Array = [
 
 	/*[59.44887, -24.32432,Natives,"<b>Test Marker</b>"],*/
-	[33.66787, 114.67181,Epitath,"<b>Epitath</b>"],
-	[40.58522, 58.37838,Pistol,"<b>Fort Major</b>"],
-	[40.58522, 68.37838,Rifles,"<b>Fort Major</b>"],
-	[40.58522, 78.37838,Cannon,"<b>Fort Major</b>"]
+	[33.66787, 114.67181,Epitaph,"<b>Epitaph</b>"],
+	[40.58522, 58.87838,FortMajor,"<b>Fort Major</b>"]
 	];
 
 
